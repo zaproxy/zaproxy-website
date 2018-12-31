1bbea39e850dfe749f334206be93eed4c4a81ebf
Jared Hardy <jared.hardy@looker.com>
2018-12-31 06:28:47 -0800

    site with a pipeline

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
+import Glide from '@glidejs/glide'
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+new Glide('.glide').mount()
