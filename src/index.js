6b81ca7baf5a6d64e4f14d4b942b912b0f1f7c49
Andres Hermosilla <an2dres5m@gmail.com>
2019-06-30 22:04:37 -0700

    Added prismjs for code highlighting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
+import "./js/prism.js";
 import Glide from '@glidejs/glide';
 import BadgerAccordion from 'badger-accordion';
 
@@ -15,3 +16,17 @@ new BadgerAccordion('.js-badger-accordion', {
   openHeadersOnLoad: [0]
 });
 
+
+/*
+dl.weekly.Stable Windows
+action = dl
+category = weekly
+label = Stable Windows
+
+ga('send', {
+  hitType: 'event',
+  eventAction: 'play',
+  eventCategory: 'Videos',
+  eventLabel: 'Fall Campaign'
+});
+*/
