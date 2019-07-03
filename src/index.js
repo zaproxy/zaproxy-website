3274c7b70d759b62b7da4c6c6e3e471be59c2211
Andres Hermosilla <an2dres5m@gmail.com>
2019-07-02 22:52:50 -0700

    Add ids to downloads for tracking

--- src/index.js
+++ src/index.js
@@ -4,22 +4,28 @@ import "./css/main.css";
 import "./js/prism.js";
 import BadgerAccordion from 'badger-accordion';
 
-
 new BadgerAccordion('.js-badger-accordion', {
   openHeadersOnLoad: [0]
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  Array.from(document.querySelectorAll('[track-event]')).map(el => el.addEventListener('click', function(e) {
+    e.preventDefault();
+    let [action, category, label] = el.getAttribute('track-event').split('.')
+    if (!window.ga) {
+      console.log("track-event", action, category, label);
+    } else {
+      ga('send', {
+        hitType: 'event',
+        eventAction: action,
+        eventCategory: category,
+        eventLabel: label,
+      });
+    }
 
-/*
-dl.weekly.Stable Windows
-action = dl
-category = weekly
-label = Stable Windows
-
-ga('send', {
-  hitType: 'event',
-  eventAction: 'play',
-  eventCategory: 'Videos',
-  eventLabel: 'Fall Campaign'
+    // Timeout to give time for the event can be tracked
+    setTimeout(() => {
+      window.location = el.href;
+    }, 400);
+  }));
 });
-*/
