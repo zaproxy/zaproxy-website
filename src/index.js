964889046d68155af0685e7c25b3c25e6704e6fa
Andres Hermosilla <an2dres5m@gmail.com>
2019-07-02 22:59:49 -0700

    Removed vulnerable npm package & cleaned up some JavaScript

--- src/index.js
+++ src/index.js
@@ -2,30 +2,32 @@
 
 import "./css/main.css";
 import "./js/prism.js";
-import BadgerAccordion from 'badger-accordion';
+import BadgerAccordion from "badger-accordion";
 
-new BadgerAccordion('.js-badger-accordion', {
+new BadgerAccordion(".js-badger-accordion", {
   openHeadersOnLoad: [0]
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-  Array.from(document.querySelectorAll('[track-event]')).map(el => el.addEventListener('click', function(e) {
-    e.preventDefault();
-    let [action, category, label] = el.getAttribute('track-event').split('.')
-    if (!window.ga) {
-      console.log("track-event", action, category, label);
-    } else {
-      ga('send', {
-        hitType: 'event',
-        eventAction: action,
-        eventCategory: category,
-        eventLabel: label,
-      });
-    }
+  Array.from(document.querySelectorAll("[track-event]")).map((el) => {
+    el.addEventListener("click", function(e) {
+      e.preventDefault();
+      const [action, category, label] = el.getAttribute("track-event").split(".")
+      if (ga === undefined) {
+        console.log("track-event", action, category, label);
+      } else {
+        ga("send", {
+          hitType: "event",
+          eventAction: action,
+          eventCategory: category,
+          eventLabel: label,
+        });
+      }
 
-    // Timeout to give time for the event can be tracked
-    setTimeout(() => {
-      window.location = el.href;
-    }, 400);
-  }));
+      // Timeout to give time for the event can be tracked
+      setTimeout(() => {
+        window.location = el.href;
+      }, 400);
+    })
+  });
 });
