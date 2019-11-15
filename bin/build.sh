7344778fa9dc1a1ba454e051242f8f141025f366
Andres Hermosilla <an2dres5m@gmail.com>
2019-11-14 22:42:08 -0800

    #9 Included copying API docs into build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -2,7 +2,23 @@
 
 set -e
 
-TARGET_DIR="${1:-./}"
+BUILD_HASH=$(git log -1 --pretty=format:"%h")
+DEFAULT_DIR="./build-${BUILD_HASH}"
+TARGET_DIR=$(realpath "${1:-$DEFAULT_DIR}")
+
+build_api_docs()
+{
+    local output="$1"
+    if [ ! -d zap-api-docs ]
+    then
+        git clone https://github.com/zaproxy/zap-api-docs
+    fi
+    cd zap-api-docs
+    git pull
+    bundler install
+    bundle exec middleman build --clean
+    cp -rf ./build/ "${output}/docs/api"
+}
 
 build_docker()
 {
@@ -18,17 +34,19 @@ build_local()
     echo '[i] Building using local nodejs'
     local output="$1"
     npm run build
-    cp -r ./dist "${output}"
+    cp -rf ./dist "${output}"
 }
 
 
 echo "[i] Starting build "
 
-if (command -v dockecr)
+if (command -v docker)
 then
     build_docker "${TARGET_DIR}"
 else
     build_local "${TARGET_DIR}"
 fi
 
-echo "[i] Output build into: "${TARGET_DIR}""
+build_api_docs "${TARGET_DIR}"
+
+echo "[i] Output build into: ${TARGET_DIR}"
